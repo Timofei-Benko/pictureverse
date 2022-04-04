23f2e29fe4bb351e274a97c148b594b24988b8ca
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2022-04-05 03:06:16 +0500

    Start migration to Tailwind

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,22 +1,16 @@
 import { useRouter } from 'next/router';
-import cn from 'classnames';
 import { Dropdown } from './components';
-
-import styles from './Header.module.sass';
 
 export const Header = () => {
   const { pathname } = useRouter();
   const isNotLoginPage = pathname !== '/login' && pathname !== '/register';
 
   return (
-    <div className={cn(
-      styles.root,
-      { [styles.regular]: isNotLoginPage }
-      )}>
-      <div className='pageContainer'>
-        <div className={styles.inner}>
-          <div className={styles.logoContainer}>
-            <span className={styles.logo}>Pictureverse</span>
+    <div className={`w-full ${isNotLoginPage && 'border-b-3 border-b-secondary'}`}>
+      <div className='page-wrap'>
+        <div className='flex items-center h-full py-3 px-6'>
+          <div className='mb-3'>
+            <span className='logo'>Pictureverse</span>
           </div>
           <Dropdown />
         </div>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: '#f5e7df',
+      secondary: '#0a1347',
+    },
+    borderWidth: {
+      '3': '3px',
+    },
+  },
+  plugins: [],
+}
